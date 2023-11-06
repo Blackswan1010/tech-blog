ab4b8f4e83bae3ee3339490efa96679ae5b1ecc2
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-06 05:39:16 -0800

    added hooks to User model

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -22,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
