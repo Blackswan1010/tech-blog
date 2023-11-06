c8369a0f3ee66f5d2c260eb9a8fc0f566262512d
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-06 04:39:14 -0800

    implemented model Comment

--- models/User.js
+++ models/User.js
@@ -1,43 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init(
     {
         id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
         username: {
-          type: DataTypes.STRING,
-          allowNull: false,
+            type: DataTypes.STRING,
+            allowNull: false,
         },
         email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            isEmail: true,
-          },
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
         },
         password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [6],
-          },
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
         },
-      },
-      {
+    },
+    {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user',
-      }
+    }
 );
 
 
