53fbe96b50ef074a972339f43d7ea50c175c30e6
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-07 14:31:31 -0800

    fixed comment routes

--- controller/api/comment-routes.js
+++ controller/api/comment-routes.js
@@ -1,42 +1,40 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
-// Getting all comments for a post
-router.get("/:id", async (req, res) => {
+// Get all comments for the post
+router.get('/:id', async (req, res) => {
     try {
-        const commentData = await Comment.findAll({
-            include: [{ model: User }, { model: Post }],
-            where: { post_id: req.params.id }
-        });
-
-        if (commentData) {
-            res.status(200).json(commentData);
-        } else {
-            res.status(404).json('Comments not found!');
-        }
-
+      const commentData = await Comment.findAll({
+        include: [{ model: Post }, { model: User}],
+        where: { post_id: req.params.id }
+      });
+      if (commentData) {
+        res.status(200).json(commentData);
+    } else {
+        res.status(404).json('Comments not found!');
+    }
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
 });
 
-// Creating a comment on a post
-router.post("/", async (req, res) => {
-    try {
-        const newComment = await Comment.create({
-            content_text: req.body.comment,
-            post_id: req.body.post_id,
-            author_id: req.session.user_id,
-        });
-
-        if (newComment) {
-            res.status(200).json(newComment);
-        } else {
-            res.status(400).json({ message: 'Failed to add comment' });
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
+// Create a comment to the specific post
+router.post('/', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      content_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      author_id: req.session.user_id,
+    });
+  if (newComment) {
+    res.status(200).json(newComment);
+  } else {
+    res.status(400).json({ message: 'Failed to add comment' });
+  }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
