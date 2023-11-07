db06926bca63e72d062215d990d34577d5486fdf
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-07 06:27:08 -0800

    adding handlebars files to be implemented

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,1 +1,60 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const postData = await Post.findAll({
+            order: [['creation_date', 'DESC']],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes:['username']
+                    }
+                }
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('project', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
